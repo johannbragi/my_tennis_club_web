d17cd4a4bee1050ce69d53f47d7c9fd420d05073
johannbragi <johannbragi@festi.is>
2023-04-16 19:22:23 +0000

    views change

--- member/views.py
+++ member/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the members index.")
+    x = "blaaaaa"
+    return HttpResponse("Hello more hello, world. You're at the members index." + x)
